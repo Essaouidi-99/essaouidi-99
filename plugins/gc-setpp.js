808547df03a1a2049d4f8964f2d4d98bc0744ec5
Essaouidi25 <160172347+Essaouidi25@users.noreply.github.com>
2024-03-10 17:24:20 +0000

    Update gc-setpp.js

--- plugins/gc-setpp.js
+++ plugins/gc-setpp.js
@@ -1,14 +1,59 @@
+import fs from 'fs'
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
 
-let handler = async (m, { conn, usedPrefix, command }) => {
-    let q = m.quoted ? m.quoted : m
-    let mime = (q.msg || q).mimetype || q.mediaType || ''
-    if (/image/.test(mime)) {
-    let img = await q.download()
-    if (!img) throw '*REPLY TO AN IMAGE.*'
-    await conn.updateProfilePicture(m.chat, img).then(_ => m.reply('IMAGE SUCCESSFULLY SET AS PP._*'))
-    } else throw '*REPLY TO AN IMAGE.*'}
-    handler.command = /^setpp(group|grup|gc)?$/i
-    handler.group = true
-    handler.admin = true
-    handler.botAdmin = true
-    export default handler
+const jimp_1 = require('jimp')
+
+let handler = async (m, { conn, command, usedPrefix }) => {
+ let q = m.quoted ? m.quoted : m
+ let mime = (q.msg ? q.msg : q).mimetype ? q.mimetype : q.mediaType || ''
+ if (/image/g.test(mime) && !/webp/g.test(mime)) {
+  try {
+   let media = await q.download()
+   let botNumber = await conn.user.jid
+   let { img } = await pepe(media)
+   await conn.query({
+    tag: 'iq',
+    attrs: {
+     to: botNumber,
+     type:'set',
+     xmlns: 'w:profile:picture'
+    },
+    content: [
+     {
+      tag: 'picture',
+      attrs: { type: 'image' },
+      content: img
+     }
+    ]
+   })
+   m.reply('تم بنجاح تغيير صورة بروفيل البوت')
+  } catch (e) {
+   console.log(e)
+   m.reply('حدث خطأ، حاول مرة أخرى لاحقًا')
+  }
+ } else {
+  m.reply(`أرسل الصورة مع التسمية التوضيحية ${usedPrefix + command} \nأو قم  بالاشارة  للصورة التي تريد وضعها كبروفايل البوت`)
+ }
+}
+
+handler.help = ['setpp']
+handler.tags = ['owner']
+handler.command = /^(setpp)$/i
+
+handler.owner = true
+
+export default handler
+
+async function pepe(media) {
+    const jimp = require('jimp');
+    const image = await jimp.read(media)
+    const min = image.getWidth()
+    const max = image.getHeight()
+    const cropped = image.crop(0, 0, min, max)
+    return {
+     img: await cropped.scaleToFit(720, 720).getBufferAsync(jimp.MIME_JPEG),
+     preview: await cropped.normalize().getBufferAsync(jimp.MIME_JPEG)
+    }
+   }
+   
